37d0f592682c3596330fdab0c5483cba07070828
Nacho <ignaciomies@live.com>
2018-09-02 15:40:19 -0300

    test1

--- models.js
+++ models.js
@@ -5,32 +5,25 @@ var sword = { damage : 20, defense : 20, value : 70};
 var bow = { damage : 30, defense : 10, value : 50};
 var weapons = [sword, bow];
 
-function buscarPersonaEnServidor(callback)
-{
-    setTimeout(function(){
-        callback({Nombre : "Ignacho"});
-    }, 1000);
-}
-
-
-
-var ListPersonas = [];
-
-function ListarPersonas(){
-    console.log("----------------------------------");
-    for(var i = 0; i < ListPersonas.length; i++)
-    {
-        console.log(ListPersonas[i].Nombre + "_" + i);
-    }
-}
-
-
-function AgregarPersona()
-{
-    buscarPersonaEnServidor(function(persona){
-        ListPersonas.push(persona);
-        ListarPersonas();
-    });
-}
-
-document.getElementById("agregarPersona").addEventListener("click",AgregarPersona);
+// function buscarPersonaEnServidor(callback)
+// {
+//     setTimeout(function(){
+//         callback({Nombre : "Ignacho"});
+//     }, 1000);
+// }
+// var ListPersonas = [];
+// function ListarPersonas(){
+//     console.log("----------------------------------");
+//     for(var i = 0; i < ListPersonas.length; i++)
+//     {
+//         console.log(ListPersonas[i].Nombre + "_" + i);
+//     }
+// }
+// function AgregarPersona()
+// {
+//     buscarPersonaEnServidor(function(persona){
+//         ListPersonas.push(persona);
+//         ListarPersonas();
+//     });
+// }
+// document.getElementById("agregarPersona").addEventListener("click",AgregarPersona);
